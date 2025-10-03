998524a5295e1db29528418b889cab19a0f27844
Felipe KiKo <felipe.kiko@gmail.com>
2025-10-02 21:52:33 -0300

    fix: response in chat

--- provider/lambdas/lambda-provider-dispatcher/index.py
+++ provider/lambdas/lambda-provider-dispatcher/index.py
@@ -1,12 +1,78 @@
 import json
-import boto3
 import os
 import uuid
+import boto3
+from botocore.config import Config
+import time
+
+def get_appconfig_flag(application, environment, configuration):
+    try:
+        client = boto3.client('appconfigdata')
+        
+        session = client.start_configuration_session(
+            ApplicationIdentifier=application,
+            EnvironmentIdentifier=environment,
+            ConfigurationProfileIdentifier=configuration,
+            RequiredMinimumPollIntervalInSeconds=15
+        )
+        
+        response = client.get_latest_configuration(
+            ConfigurationToken=session['InitialConfigurationToken']
+        )
+        
+        if 'Configuration' in response and response['Configuration']:
+            config_content = response['Configuration'].read().decode('utf-8')
+            
+            try:
+                config = json.loads(config_content)
+                service_available = config.get('isServiceAvailable', False)
+
+                return service_available['enabled']
+            except json.JSONDecodeError as e:
+                return False
+        else:
+            return False
+            
+    except Exception as e:
+        print(f"[ERROR] Error in get_appconfig_flag: {str(e)}")
+        return False
 
 def lambda_handler(event, context):
+    service_available = get_appconfig_flag(
+        os.environ['APP_CONFIG_APPLICATION'],
+        os.environ['APP_CONFIG_ENVIRONMENT'],
+        os.environ['APP_CONFIG_CONFIGURATION']
+    )
+
     connection_id = event['requestContext']['connectionId']
     domain = event['requestContext']['domainName']
     stage = event['requestContext']['stage']
+
+    if service_available == False:
+        apigw_management = None
+        message_system_down = 'Sistema em manutenção. Por favor, tente novamente mais tarde.'
+
+        if not apigw_management:
+            apigw_management = boto3.client('apigatewaymanagementapi', endpoint_url=f"https://{domain}/{stage}")
+
+        apigw_management.post_to_connection(
+            Data=json.dumps({'type': 'stream', 'chunk': message_system_down}),
+            ConnectionId=connection_id
+        )
+
+        apigw_management.post_to_connection(
+            Data=json.dumps({'type': 'end'}),
+            ConnectionId=connection_id
+        )
+
+        return {
+            'statusCode': 503,
+            'body': json.dumps({
+                'status': 'error',
+                'message': message_system_down
+            })
+        }
+
     body = event.get('body')
 
     if body and isinstance(body, str):
